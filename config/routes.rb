8fe6518f898c5253b0b0a01ea2dc33c586b8a641
Maueez Ahmed <maueezahmed@gmail.com>
2024-02-15 21:41:50 +0500

    employee api integration and view changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       post 'import'
     end
   end
+  resources :employees
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
